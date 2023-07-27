638dadcf13b24b15c1ddc11b719e00d5c7dba3b5
suhas patil <suhas.patil@salesforce.com>
2023-07-26 22:57:06 -0500

    Frontend
    Added proxy to fetch

--- assignmentsubmissionapp-frontend/src/App.js
+++ assignmentsubmissionapp-frontend/src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const reqBody = {
+    "username": "suhas",
+    "password": "123123"
+  };
+  fetch("api/auth/login", {
+    headers: {
+      "Content-Type": "application/json"
+    },
+    method: "post",
+    body: JSON.stringify(reqBody)
+  });
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello World!</h1>
     </div>
   );
 }
