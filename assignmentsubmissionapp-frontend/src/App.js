7c3521907960d24c2cf7a54b437e12184eddde6e
suhas patil <suhas.patil@salesforce.com>
2023-08-08 00:18:11 -0500

    UseState with localStorage

--- assignmentsubmissionapp-frontend/src/App.js
+++ assignmentsubmissionapp-frontend/src/App.js
@@ -1,36 +1,39 @@
 import './App.css';
-import {useEffect, useRef, useState} from "react";
+import {useEffect, useRef} from "react";
+import {useLocalState} from "./util/useLocalStorage";
 
 function App() {
   const initialized = useRef(false);
-
-  const [jwt, setJwt] = useState("");
+  const [jwt, setJwt] = useLocalState("", "jwt");
 
   useEffect(
   () => {
-    if (!initialized.current) {
-      initialized.current = true
+    if(!jwt) {
+      if (!initialized.current) {
+        initialized.current = true
 
-      const reqBody = {
-        "username": "suhas",
-        "password": "123123"
-      };
-      fetch("api/auth/login", {
-        headers: {
-          "Content-Type": "application/json"
-        },
-        method: "post",
-        body: JSON.stringify(reqBody)
-      }).then((response)=> Promise.all([response.json(), response.headers]))
-      .then(([body, headers]) => {
-        setJwt(headers.get("authorization"));
-        console.log("jwt :: "+ jwt);
-        console.log(body);
-      });
-      console.log('This is fired ');
+        const reqBody = {
+          "username": "suhas",
+          "password": "123123"
+        };
+        fetch("api/auth/login", {
+          headers: {
+            "Content-Type": "application/json"
+          },
+          method: "post",
+          body: JSON.stringify(reqBody)
+        }).then((response)=> Promise.all([response.json(), response.headers]))
+            .then(([body, headers]) => {
+              setJwt(headers.get("authorization"));
+            });
+      }
     }
   }
-  ,[jwt]);
+  ,[]);
+
+  useEffect(()=>{
+    console.log("jwt :: "+ jwt);
+  }, [jwt]);
 
   return (
     <div className="App">
