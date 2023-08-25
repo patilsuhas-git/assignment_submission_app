50a2083efbe08dc4dc7f5710311ef2f29c284a0e
suhas patil <suhas.patil@salesforce.com>
2023-08-25 08:52:18 -0500

    login func

--- assignmentsubmissionapp-frontend/src/app/App.js
+++ assignmentsubmissionapp-frontend/src/app/App.js
@@ -8,36 +8,9 @@ import Login from "../Login";
 import PrivateRoute from "../PrivateRoute";
 
 function App() {
-  const initialized = useRef(false);
   const [jwt, setJwt] = useLocalState("", "jwt");
 
-  // useEffect(
-  // () => {
-  //   if(!jwt) {
-  //     if (!initialized.current) {
-  //       initialized.current = true
-  //
-  //       const reqBody = {
-  //         "username": "suhas",
-  //         "password": "123123"
-  //       };
-  //       fetch("api/auth/login", {
-  //         headers: {
-  //           "Content-Type": "application/json"
-  //         },
-  //         method: "post",
-  //         body: JSON.stringify(reqBody)
-  //       }).then((response)=> Promise.all([response.json(), response.headers]))
-  //           .then(([body, headers]) => {
-  //             setJwt(headers.get("authorization"));
-  //           });
-  //     }
-  //   }
-  // }
-  // ,[]);
-
   useEffect(()=>{
-    console.log("jwt :: "+ jwt);
   }, [jwt]);
 
   return (
