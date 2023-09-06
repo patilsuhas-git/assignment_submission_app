ac0082769010615e55e1d4021d3ffc2552d1b036
suhas patil <suhas.patil@salesforce.com>
2023-09-06 00:09:13 -0500

    Added JWT expiry validation

--- assignmentsubmissionapp-frontend/src/PrivateRoute/index.js
+++ assignmentsubmissionapp-frontend/src/PrivateRoute/index.js
@@ -1,11 +1,33 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {useLocalState} from "../util/useLocalStorage";
 import {Navigate} from "react-router-dom";
+import ajax from "../Services/fetchService";
 
 const PrivateRoute = ({children}) => {
     const [jwt, setJwt] = useLocalState("", "jwt");
+    const [isLoading, setIsLoading] = useState(true);
+    const [isValid, setIsValid] = useState(null);
     // console.log("JWT :::: "+jwt);
-    return jwt ? children : <Navigate to="/login"/>
+    if (jwt) {
+        ajax(`/api/auth/validate?token=${jwt}`, "get", jwt).then((response) => {
+                return response.json();
+            }).then((responseData) => {
+                setIsValid(responseData.isValid);
+                setIsLoading(false);
+            });
+    } else {
+        return <Navigate to="/login"/>;
+    }
+
+    if(isLoading) {
+        return <div>Loading....</div>;
+    } else {
+        if(isValid === true) {
+            return children;
+        } else {
+            return <Navigate to="/login"/>;
+        }
+    }
 };
 
 export default PrivateRoute;
